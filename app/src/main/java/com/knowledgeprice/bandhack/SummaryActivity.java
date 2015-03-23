17a332c4e9ee8aabe0607478a36d96eeaf1a861e
Ralfs Gūtmanis <ralfsgutmanis@Gunarss-MacBook-Pro.local>
2015-03-23 23:15:33 +0200

    Default summary activity

---
+++ app/src/main/java/com/knowledgeprice/bandhack/SummaryActivity.java
@@ -0,0 +1,75 @@
+package com.knowledgeprice.bandhack;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class SummaryActivity extends ActionBarActivity {
+
+    public enum DiscountLevel { BAD, LOW, MEDIUM, HIGH }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+        setDiscountLevel(DiscountLevel.LOW);
+        setCalculatedScore(7.1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_summary, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setDiscountLevel(DiscountLevel level) {
+        String discountText = "";
+
+        switch (level) {
+            case BAD:
+                discountText = "See doctor";
+                break;
+            case LOW:
+                discountText = "See advisor";
+                break;
+            case MEDIUM:
+                discountText = "2%";
+                break;
+            case HIGH:
+                discountText = "4%";
+                break;
+            default:
+                discountText = "No info";
+                break;
+        }
+
+        TextView discountAmount = (TextView)findViewById(R.id.discountAmount);
+        discountAmount.setText(discountText);
+    }
+
+    private void setCalculatedScore(Double score) {
+        TextView calculatedScore = (TextView)findViewById(R.id.calculatedScore);
+        calculatedScore.setText(score.toString());
+    }
+}
