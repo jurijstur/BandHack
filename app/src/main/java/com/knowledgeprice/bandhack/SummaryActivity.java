f9d611470afe058afa3468ddc122e9707d2e6054
Ralfs Gūtmanis <ralfsgutmanis@Gunarss-MacBook-Pro.local>
2015-03-24 02:01:49 +0200

    Score calculation

--- app/src/main/java/com/knowledgeprice/bandhack/SummaryActivity.java
+++ app/src/main/java/com/knowledgeprice/bandhack/SummaryActivity.java
@@ -3,11 +3,14 @@ package com.knowledgeprice.bandhack;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 
 public class SummaryActivity extends ActionBarActivity {
@@ -28,8 +31,10 @@ public class SummaryActivity extends ActionBarActivity {
             }
         });
 
-        setDiscountLevel(DiscountLevel.MEDIUM);
-        setCalculatedScore(7.1);
+        long stepCount = Model.getInstance().getTotalSteps();
+        Log.v("DEZ", "stepCount: " + Long.toString(stepCount));
+        setDiscountLevel(calculateDiscountLevel(stepCount));
+        setCalculatedScore(calculateScore(stepCount));
     }
 
 
@@ -60,10 +65,10 @@ public class SummaryActivity extends ActionBarActivity {
 
         switch (level) {
             case BAD:
-                discountText = "See doctor";
+                discountText = "BAD";
                 break;
             case LOW:
-                discountText = "See advisor";
+                discountText = "0%";
                 break;
             case MEDIUM:
                 discountText = "2%";
@@ -72,7 +77,7 @@ public class SummaryActivity extends ActionBarActivity {
                 discountText = "4%";
                 break;
             default:
-                discountText = "No info";
+                discountText = "No Info";
                 break;
         }
 
@@ -80,8 +85,44 @@ public class SummaryActivity extends ActionBarActivity {
         discountAmount.setText(discountText);
     }
 
-    private void setCalculatedScore(Double score) {
+    private DiscountLevel calculateDiscountLevel(long stepCount) {
+        int[] stepCountRow = new int[] {
+                5000,
+                10000,
+                15000,
+                999999999};
+
+        DiscountLevel[] discountLevelRow = new DiscountLevel[] {
+                DiscountLevel.BAD,
+                DiscountLevel.LOW,
+                DiscountLevel.MEDIUM,
+                DiscountLevel.HIGH};
+
+        for(int i = 0; i < stepCountRow.length; i++) {
+            if(stepCount > stepCountRow[i]) {
+                continue;
+            } else {
+                return discountLevelRow[i];
+            }
+        }
+
+        return null; // TODO this never happens =]
+    }
+
+    private String calculateScore(long stepCount) {
+        int maxSteps = 15000;
+
+       BigDecimal calculatedScore = new BigDecimal(stepCount).divide(new BigDecimal(maxSteps));
+       calculatedScore.setScale(2, BigDecimal.ROUND_UP);
+       if (calculatedScore.compareTo(new BigDecimal(1.0)) > 0) {
+           return "1.00";
+       } else {
+           return calculatedScore.toString();
+       }
+    }
+
+    private void setCalculatedScore(String score) {
         TextView calculatedScore = (TextView)findViewById(R.id.calculatedScore);
-        calculatedScore.setText(score.toString());
+        calculatedScore.setText(score);
     }
 }
